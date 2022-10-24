8e16c85232b532310f21f633f39b7d0f82d0c114
Kevinsansuor <105604634+Kevinsansuor@users.noreply.github.com>
2022-10-23 19:20:40 -0500

    initial commit

---
+++ js/backto__top.js
@@ -0,0 +1,18 @@
+
+let mybutton = document.getElementById("btn__top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 35 || document.documentElement.scrollTop > 35) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+t
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
